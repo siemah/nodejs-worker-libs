6b73d8cf66684c6bc84b02a3a775365f43246a17
siemah <m_hamiche@inptic.edu.dz>
2019-10-05 17:46:09 +0100

    handle the case of array extensions passed

--- libs/workers/resizeImages.js
+++ libs/workers/resizeImages.js
@@ -2,17 +2,49 @@ let { parentPort, workerData, } = require('worker_threads');
 let sharp = require('sharp');
 
 async function resize() {
-  const { image, sizes, outputDir, imageName=Date.now(), toExtentions='png', fit="cover" } =  workerData;
-  const _shapImagesPromise = sizes.map( size => {
+  const { image, sizes, outputDir, imageName=Date.now(), toExtensions='png', fit="cover" } =  workerData;
+  let _shapImagesPromise = [];
+  sizes.map( size => {
     let { width, height } = typeof size === 'object' ? size : { width: size, height: size};
-    const outputPath  =  `${outputDir}/${imageName}-${width}x${height}`;
+    const _outputPath  =  `${outputDir}/${imageName}-${width}x${height}`;
     const _currentImage = sharp(image).resize(width, height, { fit });
-    if( !Array.isArray(toExtentions) ) {
-      return _currentImage.toFile(`${outputPath}.${toExtentions}`);
+
+    if( !Array.isArray(toExtensions) ) {
+      _shapImagesPromise = [
+        ..._shapImagesPromise, 
+        _currentImage.png().toFile(`${_outputPath}.${toExtensions}`)
+      ];
+    } else {
+      toExtensions.forEach( extension => {
+        switch (extension) {
+          case 'webp':
+            _shapImagesPromise = [
+              ..._shapImagesPromise, 
+              _currentImage.webp({ lossless: true, }).toFile(`${_outputPath}.${extension}`)
+            ];
+            break;
+          case 'jpeg':
+            _shapImagesPromise = [
+              ..._shapImagesPromise, 
+              _currentImage.jpeg().toFile(`${_outputPath}.${extension}`)
+            ];
+            break;
+          default: 
+            _shapImagesPromise = [
+              ..._shapImagesPromise, 
+              _currentImage.png().toFile(`${_outputPath}.${extension}`)
+            ];
+        }
+      });
     }
   });
 
-  _res = await Promise.all([..._shapImagesPromise]);
+  let _res
+  try {
+    _res = await Promise.all(_shapImagesPromise);
+  } catch (error) {
+    _res = error;  
+  }
   parentPort.postMessage(_res);
 }
 
