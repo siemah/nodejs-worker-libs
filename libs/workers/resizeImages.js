4fc478de90a5041a2b0d56693cd2c45f657d05e0
siemah <m_hamiche@inptic.edu.dz>
2019-10-05 14:02:57 +0100

    handle the case of toExtensions is not an array

--- libs/workers/resizeImages.js
+++ libs/workers/resizeImages.js
@@ -2,13 +2,14 @@ let { parentPort, workerData, } = require('worker_threads');
 let sharp = require('sharp');
 
 async function resize() {
-  const { image, sizes, outputDir, imageName=Date.now(), toExtention='png', fit="cover" } =  workerData;
+  const { image, sizes, outputDir, imageName=Date.now(), toExtentions='png', fit="cover" } =  workerData;
   const _shapImagesPromise = sizes.map( size => {
     let { width, height } = typeof size === 'object' ? size : { width: size, height: size};
-    const outputPath  =  `${outputDir}/${imageName}-${width}x${height}.${toExtention}`;
-    return sharp(image)
-    .resize(width, height, { fit })
-    .toFile(outputPath);
+    const outputPath  =  `${outputDir}/${imageName}-${width}x${height}`;
+    const _currentImage = sharp(image).resize(width, height, { fit });
+    if( !Array.isArray(toExtentions) ) {
+      return _currentImage.toFile(`${outputPath}.${toExtentions}`);
+    }
   });
 
   _res = await Promise.all([..._shapImagesPromise]);
